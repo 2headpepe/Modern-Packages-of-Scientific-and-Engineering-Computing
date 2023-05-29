359430466059f1f8ab7b39727165cb7898669d0c
2headpepe <st095279@student.spbu.ru>
2023-05-29 14:34:11 +0300

    normal.m

--- Normal.m
+++ Normal.m
@@ -1,4 +1,8 @@
-function retval = Normal (fun, x0, y0)
+function retval = normal (fun, x0, y0,h)
+    if(exist("h")==0)
+      h = 1e-8;
+    end
+
     x = -pi:0.01:pi;
     y = -pi:0.01:pi;
     [X,Y] = meshgrid(x,y);
@@ -6,12 +10,17 @@ function retval = Normal (fun, x0, y0)
 
 
     hold on;
-    h = 1e-8;
+    xlabel('x');
+    ylabel('y');
+    zlabel('z');
+
+    %% dx, dy - coefs for normal
     z0 = fun(x0, y0);
     dx = (fun(x0 + h, y0) - fun(x0, y0)) / h
     dy = (fun(x0, y0 + h) - fun(x0, y0)) / h
 
-    t = 0:0.01:1;
+    %% creating array of values for normal line
+    t = 0:0.01:5;
     XN = dx * t + x0;
     YN = dy * t + y0;
     ZN = -t + z0;
@@ -21,10 +30,7 @@ function retval = Normal (fun, x0, y0)
     set(S, "facealpha", 0.5)
 
 
-    xlabel('x');
-    ylabel('y');
-    zlabel('z');
+
     shading interp;
-##    facealpha (0.5);
     hold off
 endfunction
